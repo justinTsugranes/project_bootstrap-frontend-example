154206977ac0bd1e5f72de5383de1b7bc0436a14
justintsugranes <justintsugranes@proton.me>
2022-11-23 10:14:18 -0500

    run build

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css'
-import Home from './pages/Home'
+import Cards from './components/Cards'
+import Contact from './components/Contact'
+import Faq from './components/Faq'
+import Footer from './components/Footer'
+import Instructors from './components/Instructors'
+import Learn from './components/LearnOne'
+import LearnTwo from './components/LearnTwo'
+import Navigation from './components/Navigation'
+import Newsletter from './components/Newsletter'
+import Showcase from './components/Showcase'
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <Navigation />
+      <Showcase />
+      <Newsletter />
+      <Cards />
+      <Learn />
+      <LearnTwo />
+      <Faq />
+      <Instructors />
+      <Contact />
+      <Footer />
     </div>
   )
 }
